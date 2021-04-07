92140e2972f2f3f608ef19e602eded40ee101fba
gerlinp <gerlin.pierrelouis@gmail.com>
2021-04-07 03:54:36 -0400

    finishing up

--- web_app_dashboard_v3.2/js/app.js
+++ web_app_dashboard_v3.2/js/app.js
@@ -2,8 +2,9 @@ const alertBanner = document.querySelector('#alert');
 const trafficCanvas = document.querySelector('#traffic-chart')
 const dailyCanvas = document.querySelector('#daily-chart')
 const mobileCanvas = document.querySelector('#mobile-chart')
+const traffic = document.querySelector('#traffic')
 
-
+//-------------- ALERT BANNER---------------//
 alertBanner.innerHTML =
 `
 <div class="alert-banner">
@@ -18,24 +19,55 @@ alertBanner.addEventListener('click', e => {
     }
 });
 
-let trafficData = {
-    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3","4-10", "11-17", "18-24", "25-31"],
-    datasets: [{
-        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
-    2500],
-    fill: true,
-    backgroundColor: '#bb86fc',
-    borderWidth: 1,
 
+//-------------- Traffic Chart---------------//
+
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12" , "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        label: '# of Hits',
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500], 
+        fill:true,
+        backgroundColor: '#bb86fc',
+        borderWidth: 1
+    }]
+};
+
+let trafficDaily = {
+    labels: ["16-22", "23-29", "30-5", "6-12" , "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        label: '# of Hits',
+        data: [807, 1331, 2623, 2728, 2432, 1050, 588, 2676, 2381, 2990, 629], 
+        fill:true,
+        backgroundColor: '#bb86fc',
+        borderWidth: 1
+    }]
+};
+
+let trafficWeekly = {
+    labels: ["16-22", "23-29", "30-5", "6-12" , "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        label: '# of Hits',
+        data: [2460, 979, 1211, 876, 1567, 657, 1250, 1275, 2749, 2697, 770], 
+        fill:true,
+        backgroundColor: '#bb86fc',
+        borderWidth: 1
+    }]
+};
+
+let traffiMonthly = {
+    labels: ["16-22", "23-29", "30-5", "6-12" , "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        label: '# of Hits',
+        data: [1403, 1975, 1515, 866, 1988, 2207, 739, 2339, 2250, 2416, 1520], 
+        fill:true,
+        backgroundColor: '#bb86fc',
+        borderWidth: 1
     }]
 };
 
 let trafficOptions = {
-    aspectRatio: 2.5,
     responsive: true,
-    animation: {
-        duration: 5
-    },
     scales: {
         yAxes: [{
             ticks: {
@@ -55,6 +87,19 @@ let trafficChart = new Chart(trafficCanvas, {
     options: trafficOptions
 });
 
+
+// trafficData = 
+// when a radio button is clicked 
+//     checkRadioButtons
+//     return checked
+    
+traffic.addEventListener('click', e =>  {
+    let chart = e.target.innerHTML;
+    console.log(chart);    
+});
+
+
+//-------------- Daily Chart---------------//
 const dailyData = {
     labels: ["S", "M", "T", "W" , "T", "F", "S"],
     datasets: [{
@@ -84,6 +129,8 @@ let dailyChart = new Chart(dailyCanvas, {
     options: dailyOptions
 });
 
+//-------------- Daily Chart---------------//
+
 const mobileData = {
     labels: ["Desktop", "Tablet", "Phons"],
     datasets: [{
@@ -100,10 +147,11 @@ const mobileData = {
 
 const mobileOptions = {
     legend: {
-        position: 'right',
+        position: 'bottom',
         labels: {
-            boxwidth: 20,
-            fontStyle: 'bold'
+            fontColor: "white",
+            boxWidth: 20,
+            padding: 20
         }
     }
 }
