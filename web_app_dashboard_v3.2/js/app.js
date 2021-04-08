84d31d505108047f52cc75adc4143b21d991236f
gerlinp <gerlin.pierrelouis@gmail.com>
2021-04-07 23:57:05 -0400

    update

--- web_app_dashboard_v3.2/js/app.js
+++ web_app_dashboard_v3.2/js/app.js
@@ -1,23 +1,47 @@
 const alertBanner = document.querySelector('#alert');
+const notification = document.querySelector('#notification')
 const trafficCanvas = document.querySelector('#traffic-chart')
 const dailyCanvas = document.querySelector('#daily-chart')
 const mobileCanvas = document.querySelector('#mobile-chart')
 const traffic = document.querySelector('#traffic')
+const bell = document.querySelector(".notification")
+const bellItems = document.querySelectorAll('.bell-item')
+
+function close(btn) {
+    if (btn.classList.contains("clsBtn")) {
+        btn.parent.style.display = "none";
+    }
+} 
 
 //-------------- ALERT BANNER---------------//
-alertBanner.innerHTML =
-`
-<div class="alert-banner">
-<p><strong>Alert:</strong> You have overdue tasks to complete </p> <p class="alert-banner-close">X</p>
-</div>
-`
+alertBanner.innerHTML = `
+    <div class="alert-banner">
+    <p><strong>Alert:</strong> You have overdue tasks to complete </p> <p class="alert-banner-close">X</p>
+    </div>
+`;
+
+notification.innerHTML = ` 
+    <div class="notification-item" >
+        <div><p>Dale Byrd Liked ...</p><p class="clsBtn">X</p></div>
+        <div><p>Dawn Wood commented...</p><p class="clsBtn">X</p></div>
+        <div><p>Dan Oliver made a post...</p><p class="clsBtn">X</p></div>
+    </div> 
+`;
+
+
 
 alertBanner.addEventListener('click', e => {
     const element = e.target;
     if (element.classList.contains("alert-banner-close")) {
-    alert.style.display = "none"
+    alertBanner.style.display = "none"
     }
 });
+
+notification.addEventListener('click', e => {
+    close(e.target)
+});
+
+
 
 
 //-------------- Traffic Chart---------------//
@@ -33,41 +57,53 @@ let trafficData = {
     }]
 };
 
-let trafficDaily = {
-    labels: ["16-22", "23-29", "30-5", "6-12" , "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
-    datasets: [{
-        label: '# of Hits',
-        data: [807, 1331, 2623, 2728, 2432, 1050, 588, 2676, 2381, 2990, 629], 
-        fill:true,
-        backgroundColor: '#bb86fc',
-        borderWidth: 1
-    }]
-};
+let trafficUpdate = {
+    
+    monthly: {
+        labels: ['Jan','Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        datasets:[{
+            data: [1403, 1975, 1515, 866, 1988, 2207, 739, 2339, 2250, 2416, 1520], 
+            fill:true,
+            backgroundColor: '#bb86fc',
+            borderWidth: 1,
+            }]
+    },
 
-let trafficWeekly = {
-    labels: ["16-22", "23-29", "30-5", "6-12" , "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
-    datasets: [{
-        label: '# of Hits',
-        data: [2460, 979, 1211, 876, 1567, 657, 1250, 1275, 2749, 2697, 770], 
-        fill:true,
-        backgroundColor: '#bb86fc',
-        borderWidth: 1
-    }]
-};
+    weekly: {
+        labels: ["16-22", "23-29", "30-5", "6-12" , "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+        datasets:[{
+            data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+            fill:true,
+            backgroundColor: '#bb86fc',
+            borderWidth: 1,
+            }]
+    },
 
-let traffiMonthly = {
-    labels: ["16-22", "23-29", "30-5", "6-12" , "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
-    datasets: [{
-        label: '# of Hits',
-        data: [1403, 1975, 1515, 866, 1988, 2207, 739, 2339, 2250, 2416, 1520], 
-        fill:true,
-        backgroundColor: '#bb86fc',
-        borderWidth: 1
-    }]
-};
+    daily: {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        datasets:[{
+            data: [750, 1000, 1500, 1750,  1850, 1500, 2500],
+            fill:true,
+            backgroundColor: '#bb86fc',
+            borderWidth: 1,
+            }]
+    },
+    hourly: {
+        labels: ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'],
+        datasets:[{
+            data: [1331, 2623, 2728, 2432, 588, 2676, 2381, 2990, 629],
+            fill:true,
+            backgroundColor: '#bb86fc',
+            borderWidth: 1,
+            }]
+    },
+} 
 
 let trafficOptions = {
-    responsive: true,
+    aspectRation: 2.5,
+    animation: {
+        duration: 0,
+    },
     scales: {
         yAxes: [{
             ticks: {
@@ -91,16 +127,6 @@ let trafficChart = new Chart(trafficCanvas, {
 });
 
 
-// trafficData = 
-// when a radio button is clicked 
-//     checkRadioButtons
-//     return checked
-    
-// traffic.addEventListener('click', e =>  {
-//     if (Object.is(hour) )   
-// });
-
-
 //-------------- Daily Chart---------------//
 const dailyData = {
     labels: ["S", "M", "T", "W" , "T", "F", "S"],
@@ -109,7 +135,7 @@ const dailyData = {
         data: [75, 115, 175, 125, 225, 200, 100],
         backgroundColor: '#bb86fc',
         borderWidth: 1
-    }]
+    }],
 };
 
 const dailyOptions = {
@@ -125,7 +151,7 @@ const dailyOptions = {
             display: false
         }
     }
-}
+};
 
 let dailyChart = new Chart(dailyCanvas, {
     type: 'bar',
@@ -133,12 +159,11 @@ let dailyChart = new Chart(dailyCanvas, {
     options: dailyOptions
 });
 
-//-------------- Daily Chart---------------//
+//-------------- Mobile Chart---------------//
 
 const mobileData = {
-    labels: ["Desktop", "Tablet", "Phons"],
+    labels: ["Desktop", "Tablet", "Phone"],
     datasets: [{
-        label: '# of Users',
         data: [2000, 550, 500],
         borderWidth: 0,
         backgroundColor: [
@@ -153,20 +178,20 @@ const mobileOptions = {
     plugins: {
         legend: {
             position: 'right',
-            labels: {
-                fontColor: "white",
-                boxWidth: 20,
-                padding: 20
-            }
-        } 
+            align:' center'
+        }, 
     }
 }
 
-let mobileChart = new Chart(mobileCanvas,  {
+let mobileChart = new Chart(mobileCanvas, {
     type: 'doughnut',
     data: mobileData,
     options: mobileOptions
 });
+
+//-------------- Bell Notification---------------//
+
+
 
     
     
